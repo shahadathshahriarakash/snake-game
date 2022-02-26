263cfb247f19e177307ef9609fe17fc6c0dc0432
shahadathshahriarakash <shahadathshahriarakash@gmail.com>
2022-02-27 01:28:20 +0600

    Added description to main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+# Importing Lib
+
 import pygame
 from pygame.locals import *
 import time
 import random
 
+# Global size fot objects
+
 size = 20
+
+# Creating Food
 
 class Food:
     def __init__(self, surface):
@@ -12,13 +18,19 @@ class Food:
         self.x = 120
         self.y = 120
 
+    # Drawing food in display
+
     def drawApple(self):
         self.surface.blit(self.apple, (self.x, self.y))
         pygame.display.flip()
 
+    # Reappearing food after eating at random position
+
     def reAppear(self):
         self.x = random.randint(0, 24)*size
         self.y = random.randint(0, 14)*size
+
+# Creating Food
 
 class Snake:
     def __init__(self, surface, length):
@@ -29,10 +41,14 @@ class Snake:
         self.y = [size]*self.length
         self.direction = 'DOWN'
 
+    # Drawing snake
+
     def drawSnake(self):
         for i in range(self.length):
             self.surface.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
+
+    # Snakes movement
 
     def moveUP(self):
         self.direction = 'UP'
@@ -62,12 +78,19 @@ class Snake:
 
         self.drawSnake()
 
+    # Increasing snake size after eating food
+
     def incrementLenght(self):
         self.length += 1
         self.x.append(-1)
         self.y.append(-1)
 
+# Main Game
+
 class Game:
+
+    # Drawing window
+
     def __init__(self):
         pygame.init()
         pygame.display.set_caption('snakePy by shahadathshahr2')
@@ -79,9 +102,13 @@ class Game:
         self.apple = Food(self.surface)
         self.apple.drawApple()
 
+    # Adding Background Music
+
     def playBGM(self):
         pygame.mixer.music.load('resources/bgm.wav')
         pygame.mixer.music.play(-1, 0)
+
+    # Adding Sound Effects
 
     def playSoundEffects(self, soundName):
         global sound
@@ -91,6 +118,8 @@ class Game:
             sound = pygame.mixer.Sound('resources/points.wav')
 
         pygame.mixer.Sound.play(sound)
+
+    # Rendering objects
 
     def renderBGI(self):
         bg = pygame.image.load('resources/background.jpg')
@@ -105,6 +134,8 @@ class Game:
         self.surface.blit(bg, (0, 0))
         pygame.mixer.music.pause()
 
+    # Pause function
+
     def Pause(self):
         self.pause = True
         self.renderPause()
@@ -113,9 +144,13 @@ class Game:
         self.surface.blit(line, (50, 150))
         pygame.display.flip()
 
+    # Reset function for reset the game after game over
+
     def resetGame(self):
         self.snake = Snake(self.surface, 1)
         self.apple = Food(self.surface)
+
+    # Collision Rule
 
     def isCollision(self, x1, y1, x2, y2):
         if x1 >= x2 and x1 < x2 + size:
@@ -124,6 +159,8 @@ class Game:
 
         return False
 
+    # Main drawing function
+
     def draw(self):
         self.renderBGI()
         self.snake.autoWALK()
@@ -131,25 +168,34 @@ class Game:
         self.displayScore()
         pygame.display.flip()
 
+        # Snake eat food logic
+
         if self.isCollision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
             self.playSoundEffects('points')
             self.snake.incrementLenght()
             self.apple.reAppear()
+
+        # Snake touch itself logic game over logic
 
         for i in range(1, self.snake.length):
             if self.isCollision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
                 self.playSoundEffects('gameover')
                 raise 'Game Over'
 
+        # Snake touch boundary logic game over logic
+
         if not (0 <= self.snake.x[0] <= 500 and 0 <= self.snake.y[0] <= 500):
             self.playSoundEffects('gameover')
             raise "Hit the boundry error"
+
+    # Score displaying function
 
     def displayScore(self):
         font = pygame.font.SysFont('freesansbold.ttf', 30)
         score = font.render(f'{(self.snake.length - 1)}', True, (48, 71, 94))
         self.surface.blit(score, (3, 0))
 
+    # Game over displaying function
 
     def displayGameOver(self):
         self.renderGameOver()
@@ -159,12 +205,18 @@ class Game:
         pygame.mixer.music.pause()
         pygame.display.flip()
 
+    # Main run function
+
     def runGame(self):
         running = True
         self.pause = False
 
+        # Event Loop
+
         while running:
             for event in pygame.event.get():
+
+                # Setting controls
 
                 if event.type == KEYDOWN:
 
@@ -210,6 +262,8 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
+            # Finding for game over logic
+
             try:
                 if not self.pause:
                     self.draw()
@@ -220,6 +274,8 @@ class Game:
                 self.resetGame()
 
             time.sleep(0.13)
+
+# Call the main functions to run the game
 
 if __name__ == '__main__':
     game = Game()
